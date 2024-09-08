1fc67d08575caca11a0bdbc6d653fe81dd433fb4
Sophie Vu <sottvu@gmail.com>
2024-09-08 14:59:01 -0400

    git rm old css

--- src/App.vue
+++ src/App.vue
@@ -4,31 +4,10 @@ import ContactBar from './components/ContactBar.vue'
 </script>
 
 <template>
-  <header>
-    <div class="wrapper">
       <NavBar />
-      <ContactBar />      
-    </div>
-  </header>
+  <ContactBar />      
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 120vh;
-}
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
 </style>
