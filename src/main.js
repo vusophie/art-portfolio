881d3ca69dc64926d8ec4ddb2e559e4411bbe6d1
Sophie Vu <sophie_vu@otpp.com>
2024-09-29 01:29:13 -0400

    fix imports lol

--- src/main.js
+++ src/main.js
@@ -1,23 +1,19 @@
-import './assets/main.css'
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import Vue from 'vue';
-import VueI18n from 'vue-i18n';
+import './assets/main.css';
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createI18n } from 'vue-i18n';
 import en from './locales/en.json';
 
-Vue.use(VueI18n);
-const messages = {
-  en: en
-};
-const i18n = new VueI18n({
+const i18n = createI18n({
   locale: 'en',
-  messages
+  messages: {
+    en
+  }
 });
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(router)
-app.use(i18n)
-app.mount('#app')
-;
+app.use(router);
+app.use(i18n);
+app.mount('#app');
