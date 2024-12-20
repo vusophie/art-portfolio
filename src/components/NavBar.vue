c36ecf6916662ed89d0d09c62de698dd40241cc3
Sophie Vu <sottvu@gmail.com>
2024-12-20 11:27:29 -0500

    Add mobile view

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,5 +1,8 @@
 <template>    
   <nav class="navbar" :class="{ 'navbar--hidden': !showNavbar }">
+    <div class="navbar-toggle" @click="toggleNavbar">
+      <span class="toggle-icon">&#9776;</span>
+    </div>
     <RouterLink 
       v-for="(link, index) in links"
       :key="index"
@@ -23,7 +26,6 @@ export default {
   data() {
     return {
       showNavbar: true,
-      lastScrollPosition: 0,
       activeIndex: 0,
       links: [
         { text: 'Sophie Illustrations [/]', route: '/' },
@@ -35,33 +37,41 @@ export default {
   },
   methods: {
     setActiveLink(index) {
-      // console.log(index);
-      // if (index == 3) {
-        // this.showContactModal = true;
-      // } else {
-        this.activeIndex = index;
-
-      // }
+      this.activeIndex = index;
     },
-  },
+    toggleNavbar() {
+      this.showNavbar = !this.showNavbar;
+    }
+  }
 };
 </script>
 
 <style>
 
 .navbar {
-  width: 40vw;
+  width: 100%;
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
+  justify-content: center;
+  align-content: center;
   padding: 2% 5%;
   border-radius: 55px;
   margin: 2% 4%;
   font-size: 1em;
-  position: fixed; 
-  top: 0; 
-  z-index: 1000; 
-  transition: background-color 0.3s ease; 
+  position: fixed;
+  top: 0;
+  z-index: 1000;
+  transition: background-color 0.3s ease;
+}
+
+.navbar-toggle {
+  display: none;
+  cursor: pointer;
+}
+
+.toggle-icon {
+  font-size: 1.5em;
+  color: #1f2f76;
 }
 
 .sublink {
@@ -80,9 +90,43 @@ export default {
 }
 
 .sublink.active {
-  background-color: rgba(255, 255, 255, 0.55); 
-  border-radius: 25px; 
+  background-color: rgba(255, 255, 255, 0.55);
+  border-radius: 25px;
   padding: 10px 20px;
 }
 
+/* Mobile view */
+@media (max-width: 768px) {
+  .navbar {
+    flex-direction: column;
+    align-items: center;
+    padding: 1.5rem;
+    margin: 0;
+  }
+
+  .navbar-toggle {
+    display: block;
+    margin-bottom: 1rem;
+  }
+
+  .sublink {
+    font-size: 1.2em;
+    padding: 10px;
+    text-align: center;
+    width: 100%;
+  }
+
+  .navbar--hidden {
+    display: none;
+  }
+
+  .navbar-toggle + .sublink {
+    display: none;
+  }
+
+  .navbar-show .sublink {
+    display: block;
+    margin: 0.5rem 0;
+  }
+}
 </style>
