8071fc0050e0400846decf5d4bcc315cfaf14d1d
Sophie Vu <sottvu@gmail.com>
2025-01-09 20:49:42 -0500

    add books feature

--- src/views/SketchbookView.vue
+++ src/views/SketchbookView.vue
@@ -1,10 +1,20 @@
 <template>
   <div class="sketchbook">
+    <h1>CLICK ON A SKETCHBOOK TO VIEW BY BOOK!</h1>
     <div class="button-group">
-      <v-btn variant="tonal" @click="setActiveTab('dreams')">DREAMS</v-btn>
-      <v-btn variant="tonal" @click="setActiveTab('fantasy')">FANTASY</v-btn>
-      <v-btn variant="tonal" @click="setActiveTab('portrait')">PORTRAIT</v-btn>
-      <v-btn variant="tonal" @click="setActiveTab('pixel')">PIXEL</v-btn>
+      <div
+        v-for="tab in tabs"
+        :key="tab.name"
+        @click="setActiveTab(tab.name)"
+        class="tab"
+        :class="{ active: activeTab === tab.name }"
+      >
+        <div class="image-container">
+          <img :src="tab.image" />
+          <div class="hover-overlay"></div>
+        </div>
+        <p>{{ activeTab === tab.name ? `* ${tab.label} *` : tab.label }}</p>
+      </div>
     </div>
     <GalleryList :initialTab="activeTab" />
   </div>
@@ -21,6 +31,12 @@ export default {
   data() {
     return {
       activeTab: 'dreams', // Default tab
+      tabs: [
+        { name: 'dreams', label: 'DREAMS', image: '../../public/photos/dreams.png' },
+        { name: 'fantasy', label: 'FANTASY', image: '../../public/photos/fantasy.png' },
+        { name: 'portrait', label: 'PORTRAIT', image: '../../public/photos/portrait.png' },
+        { name: 'pixel', label: 'PIXEL', image: '../../public/photos/pixel.png' },
+      ],
     };
   },
   methods: {
@@ -40,12 +56,70 @@ export default {
   align-items: center;
   justify-content: center;
   margin-top: 25%;
+
+  h1 {
+    font-family: "Roboto Mono", monospace;
+    margin: 2% 0 5% 0;
+    color: darkgrey;
+    font-weight: 400;
+    font-size: 1.2em;
+  }
 }
 
 .button-group {
   display: flex;
   gap: 1em;
-  margin-bottom: 2em;
+  margin-bottom: 8vh;
+}
+
+.tab {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  cursor: pointer;
+  position: relative;
+  text-align: center;
+}
+
+.image-container {
+  position: relative;
+  width: 14vw;
+}
+
+img {
+  width: 100%;
+  height: auto;
+  transition: transform 0.3s ease;
+}
+
+img:hover {
+  transform: scale(1.05);
+}
+
+.hover-overlay {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.33);
+  opacity: 0;
+  transition: opacity 0.3s ease;
+}
+
+.image-container:hover .hover-overlay {
+  opacity: 1;
+}
+
+p {
+  font-size: 1.2em;
+  font-family: "Roboto Mono", monospace;
+  margin-top: 0.5em;
+}
+
+.tab.active p {
+  font-weight: bold;
+  color: #5790f4;
 }
 
 /* Media Queries for Mobile Responsiveness */
@@ -58,6 +132,10 @@ export default {
   .sketchbook {
     padding: 2em 1.5em;
   }
+
+  img {
+    width: 20vw;
+  }
 }
 
 @media (max-width: 480px) {
@@ -68,6 +146,10 @@ export default {
 
   .sketchbook {
     padding: 1.5em 1em;
+  }
+
+  img {
+    width: 60vw;
   }
 }
 </style>
